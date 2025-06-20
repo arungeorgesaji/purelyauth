c0d88e565c95069ff11441f4e37e96e2ffccc592
arungeorgesaji <arungeorgesaji@gmail.com>
2025-06-20 21:45:07 +0530

    Decided to make purelyauth completely foss

--- purelyauth.sql
+++ purelyauth.sql
@@ -1,11 +1,8 @@
 create table profiles (
   email text primary key not null,
   encrypted_password text not null,
-  is_pro boolean not null default false,
-  is_business boolean not null default false,
   max_applications int not null default 1,
   current_applications int not null default 0,
-  subscription_id text not null default ''
 );
 
 create table applications (
